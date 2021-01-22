ceae26ab7cffc10d68890ad465c615dda0c3c546
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-01-22 22:43:58 +0100

    Issuse 5 done

--- method/src/Sample.java
+++ method/src/Sample.java
@@ -4,16 +4,34 @@
  * {@link = method.Sample#variables}
  * {@docRoot}
  */
-
 public class Sample {
+
+    private final static int MAX_VARIABLES = 3;
     private static String example;
     private static int variables;
     private static char aChar;
     private static boolean aBoolean;
-    private static double aDouble;
-
+    private static double aDouble = 3.3;
 
     public static void main(String[] args) {
+        /**
+         * @apiNote static variable example to three object Sample give it's same
+         */
+        String example1 = Sample.example; // jeden egzemplarz składowej statycznej example
+        String example2 = Sample.example;
+        String example3 = Sample.example;
+        System.out.println("example1 = " + example1);
+        System.out.println("example2 = " + example2);
+        System.out.println("example3 = " + example3);
+
+        double aDouble = Sample.aDouble;
+        double aDouble1 = Sample.aDouble;
+        double aDouble2 = Sample.aDouble;
+        System.out.println("aDouble = " + aDouble);
+        System.out.println("aDouble1 = " + aDouble1);
+        System.out.println("aDouble2 = " + aDouble2);
+
+
         /**
          *  @return default values
          */
