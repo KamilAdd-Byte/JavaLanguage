43e35c5b10750fa0876cc7e70f0b48bede62d7dd
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-01-24 19:48:25 +0100

    Random numbers and operators with java #9

---
+++ method/src/com/responsywnie/random/MathOps.java
@@ -0,0 +1,30 @@
+package com.responsywnie.random;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Random;
+
+public class MathOps {
+    public static void main(String[] args) {
+
+        Random random = new Random(47);
+        LocalTime toLocalTime = getToLocalTime();
+
+        int i,j,k;
+        j = random.nextInt(100)+1;
+        System.out.println("od 1 do 100: " + j + " wygenerowano: " + toLocalTime);
+
+        k = random.nextInt(200)+1;
+        System.out.println("Zakres od 0 do 200: " + k + " wygenerowano: " + toLocalTime);
+
+        i = j + k;
+        System.out.println("J + K = " + i);
+
+    }
+
+    private static LocalTime getToLocalTime() {
+        LocalDateTime localDate = LocalDateTime.now();
+        LocalTime toLocalTime = localDate.toLocalTime();
+        return toLocalTime;
+    }
+}
